dcac883b535de2f88e3f2f289d1e5ae1340397db
Daniil <mrkenay@mail.ru>
2025-04-10 20:08:58 +0300

    feat: added feature create a task

---
+++ src/componets/CreateTask/CreateTask.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import styles from './CreateTask.module.scss'
+import Button from '../Button/Button';
+import { FaPlus } from 'react-icons/fa6';
+import { ITodo } from "./types";
+
+export const CreateTask = ({setTodos}: ITodo) => {
+    const [name, setName] = useState<string>('');
+    // const [status, setStatus] = useState<'todo'|'in progress'| 'done'>();
+    // const [priority, setPriority] = useState<'low' | 'medium' | 'high'>();
+    const [isOpenDetails, setIsOpenDetails] = useState<boolean>(false)
+
+    const onKeyPressNameHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+
+        if (e.key === 'Enter'){
+            if(name.trim() === '') return
+            setTodos(prev => {
+            const newTodos = [...prev, {_id: prev.length, name: name, isChecked: false}];
+            localStorage.setItem('todo', JSON.stringify(newTodos));
+            return newTodos;
+            });
+            setName('')
+        }
+    }    
+
+    return (
+        <>
+            <div className={styles.newTask}>
+                    <Button onClick={() => setIsOpenDetails(!isOpenDetails)}>
+                        <FaPlus color="black"/>
+                    </Button>
+                    <span className={styles.newTask__title}>Create new task</span>
+            </div>    
+            <div className={`${styles.detailsTask} ${isOpenDetails ? styles.active: ''}`}>
+                <div className={styles.detailsTask__createWrapper}>
+                    <input 
+                        className={styles.newTask__input} 
+                        type="text" 
+                        value={name} 
+                        onChange={e => setName(e.target.value)}
+                        onKeyDown={onKeyPressNameHandler}
+                        placeholder="Create new task" 
+                    />                       
+                </div>     
+                {/* <Button className="taskSave">Save</Button>          */}
+            </div>    
+        </>
+    );
+};
