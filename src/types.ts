b79145029982eab96904d7e53ca025b7e959ea0f
Daniil <mrkenay@mail.ru>
2025-04-11 12:06:01 +0300

    fix: fixed a types

--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,8 @@ export interface ITodoItem {
     _id: number;
     name: string;
     isChecked: boolean;
-    priority: string;
-    status?: string;
+    priority: string | null;
+    status?: string | null;
 }
 export interface IProps{
     todo: ITodoItem;
