8e51a0f8bb0f4b58b646e02a40693abe0c11de9e
Daniil <mrkenay@mail.ru>
2025-04-12 20:48:58 +0300

    feat: added a element Filter

--- src/componets/Todo/Todo.tsx
+++ src/componets/Todo/Todo.tsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import { ToDoItem } from "./ToDoItem";
 import styles from './Todo.module.scss'
 import { CreateTask } from "../CreateTask/CreateTask";
+import { CiFilter } from "react-icons/ci";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 import { ITodoItem } from "../../types";
+import Button from "../Button/Button";
 
 export const ToDo = () => {
     const [todos, setTodos] = useState<ITodoItem[]>([])
-    
+    const [isOpenFilter, setIsOpenFilter] = useState<boolean>(false);
     useEffect(() =>{
     const storedTodos = localStorage.getItem('todo')
         if(storedTodos){
@@ -39,6 +42,18 @@ export const ToDo = () => {
         <>
             <div className={styles.todoList}>
                 <h1>Todo App</h1>
+                <div className={styles.sorted}>
+                    <span className={styles.title}>Filter</span>
+                    <Button className="basic" onClick={() => setIsOpenFilter(!isOpenFilter)}>
+                        <CiFilter size={20}/>
+                    </Button>
+                    <ul className={`${styles.sorted__wrapper} ${isOpenFilter ? styles.active: ''}`}>
+                        <li className={styles.sorted__item}>Status<Button className="arrow"><FaArrowUp  size={16}/></Button></li>
+                        <li className={styles.sorted__item}>Status<Button className="arrow"><FaArrowDown  size={16}/></Button></li>
+                        <li className={styles.sorted__item}>Priority<Button className="arrow"><FaArrowUp  size={16}/></Button></li>
+                        <li className={styles.sorted__item}>Priority<Button className="arrow"><FaArrowDown  size={16}/></Button></li>
+                    </ul>
+                </div>
                 {todos.map(todo => 
                 <ToDoItem 
                 key={`_todo_${todo._id}`} 
