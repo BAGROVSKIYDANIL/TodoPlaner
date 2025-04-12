a77e93619f61842465095d7ce2d9c0923a3afa38
Daniil <mrkenay@mail.ru>
2025-04-12 13:50:20 +0300

    feat: added a feature editTask

--- src/componets/Todo/ToDoItem.tsx
+++ src/componets/Todo/ToDoItem.tsx
@@ -1,12 +1,17 @@
 import styles from './Todo.module.scss'
 import { Priority } from '../../types';
 import { FaRegTrashCan } from "react-icons/fa6";
+import { MdModeEditOutline } from "react-icons/md";
+import { FaCheck } from "react-icons/fa6";
+import { BsThreeDotsVertical } from "react-icons/bs";
 import { useState } from 'react';
 import Button from '../Button/Button';
 import { ITodoItemProps } from './inreface';
 
-export const ToDoItem:React.FC<ITodoItemProps> = ({todo, deleteTask}) => {
+export const ToDoItem:React.FC<ITodoItemProps> = ({todo, deleteTask, editTask}) => {
     const [isChecked, setIsChecked] = useState(false);
+    const [isEditTask, setIsEditTask] = useState<boolean>(false)
+    const [textFiled, setTextFiled] = useState<string>('')
     const priorityClasses: Record<Priority, string>  = {
         high: styles.priorityHigh,
         medium: styles.priorityMedium,
@@ -18,7 +23,18 @@ export const ToDoItem:React.FC<ITodoItemProps> = ({todo, deleteTask}) => {
             const checked = e.target.checked;
             setIsChecked(checked)
     }
-        
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => 
+    {
+        setTextFiled(e.target.value);
+    };        
+    const handleEditMesage = () => {
+        setTextFiled(todo.name)
+        setIsEditTask(true)
+    }
+    const successEditMessage = () => {
+            setIsEditTask(false)
+            editTask(todo._id, textFiled)
+    }
     return (
         <div className={styles.todoItem}>
                 <label key={todo._id} className={styles.checkbox} onClick={(e) => e.stopPropagation()}>
@@ -29,12 +45,32 @@ export const ToDoItem:React.FC<ITodoItemProps> = ({todo, deleteTask}) => {
                         </svg>
                     </span>
                 </label>                 
-                <span className={styles.text}>{todo.name}</span>
+                {isEditTask ?
+                    <>
+                        <input
+                            type="text"
+                            className={styles.editInput}
+                            value={textFiled}
+                            onChange={handleInputChange}
+                            autoFocus/>                      
+                        <Button onClick={() => successEditMessage()}>
+                            <FaCheck size={20} color='#00FF00'/>
+                        </Button>                    
+                    </>
+                    :
+                    <span className={styles.text}>{todo.name}</span>
+                }
                 <div className={styles.detailsTask}>
                     <span className={`${styles.detailsTask__priority} ${todo.priority !== null ? priorityClasses[todo.priority] : ''}`}>{todo.priority}</span>
                     <div className={styles.detailsTask__status}></div>
-                    <Button onClick={() => deleteTask(todo._id)}>
+                    <Button className='basic' onClick={() => handleEditMesage()}>
+                        <MdModeEditOutline size={20}/>
+                    </Button>
+                    <Button className='basic' onClick={() => deleteTask(todo._id)}>
                         <FaRegTrashCan size={20}/>
+                    </Button>
+                    <Button className='basic' >
+                        <BsThreeDotsVertical size={20}/>
                     </Button>
                 </div>
         </div>
