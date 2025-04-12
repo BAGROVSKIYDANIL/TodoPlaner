a4241c2af18accf3ec45a82c8a9cbfa425efee41
Daniil <mrkenay@mail.ru>
2025-04-12 15:42:51 +0300

    fix: fixed a types

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,11 @@
 export type Priority = 'low'|'medium'|'high' 
+export type Status = 'todo'|'in progress'| 'done'
 export interface ITodoItem {
     _id: number;
     name: string;
     isChecked: boolean;
     priority: Priority | null;
-    status?: string | null;
+    status?: Status | null | undefined;
 }
 export interface IProps{
     todo: ITodoItem;
