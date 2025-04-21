6989036e3c477c9fae0c9cd6c0e8a80e8bf62d69
Daniil <mrkenay@mail.ru>
2025-04-21 14:27:30 +0300

    feat: added a new type "SortType"

--- src/componets/Todo/TodoInreface.ts
+++ src/componets/Todo/TodoInreface.ts
@@ -1,8 +1,9 @@
 import { ITodoItem } from "../../types";
-
+export type SortType = 'order' | 'priority-asc' | 'priority-desc';
 export interface ITodoItemProps{
     todo: ITodoItem;
     deleteTask: (index: string) => void;
     editTask: (i: string, newName: string) => void
     setTodos: (prev: ITodoItem[] | ((prev: ITodoItem[]) => ITodoItem[])) => void
+    setActiveSort: (type: SortType) => void
 }
