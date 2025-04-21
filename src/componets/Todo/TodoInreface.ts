0958f26d2e582bd567b907560b55f2c07229aab1
Daniil <mrkenay@mail.ru>
2025-04-21 12:33:12 +0300

    feat: added a new Props functoin setTodos

--- src/componets/Todo/TodoInreface.ts
+++ src/componets/Todo/TodoInreface.ts
@@ -4,4 +4,5 @@ export interface ITodoItemProps{
     todo: ITodoItem;
     deleteTask: (index: string) => void;
     editTask: (i: string, newName: string) => void
+    setTodos: (prev: ITodoItem[] | ((prev: ITodoItem[]) => ITodoItem[])) => void
 }
